1d89f91eb9e494aae84399bddafc315e07f64a0b
Chi Zhang <zhangchi314@gmail.com>
2017-02-11 20:43:35 -0600

    Add files via upload

--- scr/Prior.cpp
+++ scr/Prior.cpp
@@ -1,110 +1,67 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <cmath>
-#include <map>
+#include <random>
 
 #include <Eigen/Dense>
 
-#include "World.h"
-#include "Obj_fn.h"
-#include "Optimizer.h"
 #include "Prior.h"
-#include "Controller.h"
 
-using namespace std;
-using namespace Eigen;
-// using namespace optimization;
-
-template<typename T> Obj_fn* def_objective_function(int dim) { return new T(dim); }
-
-typedef map<string, Obj_fn*> Obj_fn_map;
+// using namespace std;
 
 
+namespace optimization{
 
-int main(){
+	using namespace Eigen;
 
-	/* Fix important parameters */
-	const double start = 0.0;
-	const double stop  = 0.100;
-	const double step  = 0.001;
-	const int dim = 2;
-	const int num_particles = 200;
-	
-	// Initial distribution (Gaussian)
-	const string init_dist("gaussian");
-	const VectorXd init_mean = VectorXd::Constant(dim, 40);
-	const VectorXd init_std = VectorXd::Constant(dim, 20); // Assume uncorrelated components
-	// Initial distribution (Uniform)
-	const VectorXd l_range = VectorXd::Constant(dim, -50);
-	const VectorXd r_range = VectorXd::Constant(dim, 50);
+	/* Gaussian definitions */
 
-	const string optimizer_name ("CPF"); // Optional
+	MatrixXd Gaussian::sample_IC(const int num_particles, std::default_random_engine &generator) {
 
-	/* Initialize the world */
-	World world(start, stop, step, dim);
-	cout << world;
+		std::cout << "Generating Gaussian samples..." << std::endl;
 
-	Obj_fn_map obj_map;
-	obj_map["Rastrigin"] = def_objective_function<Rastrigin>(dim);
-	obj_map["Quadratic"] = def_objective_function<Quadratic>(dim);
+		MatrixXd particles(mean.size(), num_particles);
+		for (int d=0; d<mean.size(); d++){
+			std::normal_distribution<double> normal(mean(d), std(d));
+			RowVectorXd particles_d(num_particles);
+			for (int i=0; i<num_particles; i++){
+				particles_d(i) = normal(generator);
+			}
+			particles.row(d) = particles_d;
+		}
 
-	/* Define objective function */
-	const string obj_name("Quadratic");
-	
-	// Rastrigin rastrigin(dim);
-	// Obj_fn *fn = &rastrigin;
+		return particles;	
+	}
 
-	Obj_fn *fn = obj_map[obj_name];
-	cout << fn;
-
-	world.add_obj(fn);
-	cout << world.get_obj();
-
-	/* Define Prior model */
-	// Need to use kwargs to create a map!
+	void Gaussian::message() {
+		std::cout << "Initial distribution is Gaussian" << '\n';
+	}
 
 
-	Gaussian gaussian(init_mean, init_std);
-	Prior *prior = &gaussian;
 
-	prior->message(); // Print name of initial distribution
+	/* Uniform definitions */
 
+	MatrixXd Uniform::sample_IC(const int num_particles, std::default_random_engine &generator) {
 
-	/* Define Controller model */
-	CPF cpf(world.get_time());
-	Controller *controller = &cpf;
+		std::cout << "Generating Uniform samples..." << std::endl;
 
+		MatrixXd particles = MatrixXd::Zero(l.size(), num_particles); // Needs to prescribe the size of particles
+		for (int d=0; d<l.size(); d++){
+			std::uniform_real_distribution<double> uniform(l(d), r(d));
+			RowVectorXd particles_d(num_particles);
+			for (int i=0; i<num_particles; i++){
+				particles_d(i) = uniform(generator);
+			}
+			particles.row(d) = particles_d;
+		}
 
-	/* Define optimizer */
-	Optimizer optimizer_CPF(&world, num_particles, optimizer_name);
-	cout << optimizer_CPF;
+		return particles;	
+	}
 
-	world.add_optimizer(&optimizer_CPF);
-
-	// Set prior model of optimizer
-	optimizer_CPF.setPrior(prior);
-	optimizer_CPF.initialize();
-
-	// Set controller model of optimizer
-	// optimizer_CPF.setController(controller);
-	optimizer_CPF.setController(&cpf);
-	
-
-	/* Simulate */
-	world.simulate();
-
-	// Assess performance
-	vector<RowVectorXd> performance = controller->performance();
-	RowVectorXd h_hat = performance[0];
-	cout << h_hat << '\n';
-	// cout << h_hat(world.get_time()-1) << '\n';
+	void Uniform::message() {
+		std::cout << "Initial distribution is Uniform" << '\n';
+	}
 
 
-	delete fn;
-	fn = nullptr;
+} // End of namespace optimization
 
-	return 0;
-}
 
 
