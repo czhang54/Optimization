bcb6f84ce81b7db00eca59f0cdd7ca97b16968a6
Chi Zhang <zhangchi314@gmail.com>
2017-02-23 00:01:26 -0600

    Add files via upload

--- example/example.cpp
+++ example/example.cpp
@@ -1,12 +1,18 @@
-/* This is a simulation tool of global optimization algorithms.  
-One example is the controlled particle filter (CPF) algorithm that I have recently worked on. 
-The research paper can be found on arXiv: https://arxiv.org/abs/1701.02413. 
 
-The code structure is flexible for the user to add:
+/* This is an example program to simulate various global optimization algorithms. 
+The purpose is to illustrate how to use the algorithms and assess their performance. 
+Algorithms include: 
+  + Simulated annealing (SA)
+  + Cross entropy (CE)
+  + Particle filter (PF)
+  + Controlled particle filter (CPF)
+The controlled particle filter (CPF) algorithm is recently propose in my paper  
+which can be found on arXiv: https://arxiv.org/abs/1701.02413. 
+
+The entire project is expandable so that the user can add more:
 (1) objective functions to be minimized (see obj_map below and also src/Obj_fn.cpp)
-(2) optimization algorithms (src/Algorithms/XXXX.cpp)
+(2) optimization algorithms (scr/Algorithm.cpp)
 (3) initialization methods (src/Prior.cpp)
-Hence, the project can be easily expanded to accommodate more options. 
 */
 
 #include <iostream>
@@ -19,7 +25,9 @@ Hence, the project can be easily expanded to accommodate more options.
 #include "Prior.h"
 #include "Optimizer.h"
 #include "Algorithm.h"
+#include "SimulatedAnnealing.h"
 #include "CrossEntropy.h"
+#include "ParticleFilter.h"
 #include "ControlledParticleFilter.h"
 #include "PerformanceMetric.h"
 
@@ -35,11 +43,11 @@ typedef std::map<std::string, optimization::Obj_fn*> Obj_fn_map;
 int main(){
 
 	/* Set simulation parameters */
-	const double stop = 0.010; // Time length of the simulation
+	const double stop = 0.020; // Time length of the simulation
 	const double step_size  = 0.001; // Step size of the simulation
 	int num_iterations = static_cast<int>(stop/step_size); // Total number of iterations in the simulation
 	const int dim = 2; // Dimension of the optimization problem
-	const int num_particles = 100; // Used for particle-based methods; For others, num_particles = 1
+	const int num_particles = 500; // Used for particle-based methods; For others, num_particles = 1
 
 	/* Define a random number generator. 
 	It is initialized only once and used everywhere in the project. 
@@ -67,17 +75,29 @@ int main(){
 	std::cout << obj_fn;
 
 	/* Define and initialize optimizers */
-	// Controlled Particle Filter (CPF) algorithm
-	optimization::Optimizer<optimization::Gaussian, optimization::ControlledParticleFilter> optimizer_CPF("CPF", obj_fn, num_iterations, step_size, dim, num_particles);
-	optimizer_CPF.initialize(generator);
+
+	// Simulated Annealing (SA) algorithm
+	optimization::Optimizer<optimization::Gaussian, optimization::SimulatedAnnealing> optimizer_SA("SimulatedAnnealing", obj_fn, num_iterations, step_size, dim);
+	optimizer_SA.initialize(generator);
 
 	// Cross Entropy (CE) algorithm
 	optimization::Optimizer<optimization::Gaussian, optimization::CrossEntropy> optimizer_CE("Cross Entropy", obj_fn, num_iterations, step_size, dim, num_particles);
 	optimizer_CE.initialize(generator);
 
-	/* Simulate optimizers */
-	optimizer_CPF.simulate(generator);
+	// Particle Filter (PF) algorithm
+	optimization::Optimizer<optimization::Gaussian, optimization::ParticleFilter> optimizer_PF("Particle Filter", obj_fn, num_iterations, step_size, dim, num_particles);
+	optimizer_PF.initialize(generator);
+	
+	// Controlled Particle Filter (CPF) algorithm
+	optimization::Optimizer<optimization::Gaussian, optimization::ControlledParticleFilter> optimizer_CPF("Controlled Particle Filter", obj_fn, num_iterations, step_size, dim, num_particles);
+	optimizer_CPF.initialize(generator);
+
+
+	/* Simulate the above optimizers */
+	optimizer_SA.simulate(generator);
 	optimizer_CE.simulate(generator);
+	optimizer_PF.simulate(generator);
+	optimizer_CPF.simulate(generator);
 
 	/* Investigate performance using one of the three metrics:
 	(1) AVERAGE_VALUE: Average function value at each iterations
@@ -88,6 +108,8 @@ int main(){
 	optimizer_CE.printPerformance(BEST_VALUE_FOUND);
 	optimizer_CPF.printPerformance(DISTANCE_ERROR);
 	optimizer_CE.printPerformance(DISTANCE_ERROR);
+	optimizer_PF.printPerformance(DISTANCE_ERROR);
+	optimizer_SA.printPerformance(DISTANCE_ERROR);
 
 	delete obj_fn;
 
